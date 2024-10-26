998e1757e738f2cfbb6a35e0e4d5f70e1a47bcf8
Sozark <121688154+Sozark@users.noreply.github.com>
2024-10-26 14:30:38 -0500

    Added animations and started animating buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const element = document.getElementById('enter-button');
+const intro = document.getElementById('intro');
+
+element.addEventListener('click', () => {
+    intro.classList.add('fade-out');
+});
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "flex") {
@@ -6,3 +13,4 @@ function myFunction() {
       x.style.display = "flex";
     }
   }
+ 
